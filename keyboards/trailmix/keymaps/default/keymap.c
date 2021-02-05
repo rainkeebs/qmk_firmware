40d52c4eff4c32ad3fe47e310845bc2f194305f4
rainsff <44819800+rainsff@users.noreply.github.com>
2021-02-05 14:42:13 -0800

    dunno

--- keyboards/trailmix/keymaps/default/keymap.c
+++ keyboards/trailmix/keymaps/default/keymap.c
@@ -1,44 +1,59 @@
+/* Copyright 2021 Regan Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "solecito.h"
 
 enum layers {
   _BASE,
-  _TOGGLE,
   _LOWER,
-  _RAISE
+  _RAISE,
+  _ADJUST
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define TOGGLE TG(_TOGGLE)
+#define ADJUST MO(_ADJUST)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_BASE] = LAYOUT(
-		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
-		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENTER, 
-		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_PSCREEN, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPACE, 
+		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENTER,
+		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_PSCREEN, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPACE,
 		KC_LCTRL, KC_LALT, KC_LGUI, RAISE , KC_LSHIFT, KC_SPC, LOWER, KC_RGUI, KC_RALT, KC_RCTRL
 		),
 
-    [_TOGGLE] = LAYOUT(
-		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, 
-		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENTER, 
-		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_PSCREEN, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPACE, 
-		KC_LCTRL, KC_LALT, TOGGLE, RAISE, KC_SPC, KC_LSHIFT, LOWER, TOGGLE, KC_RALT, KC_RCTRL
-		),
-
 	[_LOWER] = LAYOUT(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_DELETE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+		KC_TILD, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_LCBR, KC_RCBR, KC_COLN, KC_DQT, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE, KC_UNDS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TOGGLE, KC_TRNS, KC_TRNS
 		),
 
 	[_RAISE] = LAYOUT(
-		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, 
+		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
 		KC_ESC, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_TRNS,
-		KC_TAB, KC_CAPS, KC_GRV, KC_BSLASH, KC_MINUS, TOGGLE, KC_TRNS, KC_EQL, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TAB, KC_CAPS, KC_DELETE, KC_BSLASH, KC_MINUS, KC_TRNS, KC_TRNS, KC_PLUS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RGUI, KC_TRNS, KC_TRNS
-		)
+		),
+
+    [_ADJUST] = LAYOUT(
+		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+		KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_ENTER,
+		KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_PSCREEN, KC_N, KC_M, KC_COMM, KC_DOT, KC_BSPACE,
+		KC_LCTRL, KC_LALT, TOGGLE, RAISE, KC_SPC, KC_LSHIFT, LOWER, TOGGLE, KC_RALT, KC_RCTRL
+		),
 };
 
 
@@ -53,7 +68,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    
+
 
 	switch (get_highest_layer(layer_state)) {
         case _BASE:
