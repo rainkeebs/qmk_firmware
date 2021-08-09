3944e09060df3e9e8a5f34191be2892e70680eb3
rainkeebs <44819800+rainkeebs@users.noreply.github.com>
2021-08-09 14:48:21 -0700

    Update config.h

--- keyboards/thevankeyboards/minivan/config.h
+++ keyboards/thevankeyboards/minivan/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID      0x8844
 #define DEVICE_VER      0x0002
 #define MANUFACTURER    Evan Sailer
-#define PRODUCT         Minivan
+#define PRODUCT         Minisub
 
 /* key matrix size */
 #define MATRIX_ROWS 4
